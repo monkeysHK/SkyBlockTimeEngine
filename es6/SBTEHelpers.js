8d0350f94234a71ffd0337ffac2144c99b6c2402
monkeyshk <monkeyshk-main@protonmail.com>
2023-01-09 19:00:10 +0800

    version beta 2

--- es6/SBTEHelpers.js
+++ es6/SBTEHelpers.js
@@ -3,7 +3,7 @@
  * @author MonkeysHK <https://github.com/MonkeysHK>
  * @description A web time engine for the time system in Hypixel SkyBlock.
  * @license GPL-3.0-or-later GNU General Public License v3.0 or later <https://www.gnu.org/licenses/gpl-3.0-standalone.html>
- * @version beta
+ * @version beta_2
  */
 /**
  * **SBTEHelpers**
--- es6/SkyRoutine.js
+++ es6/SkyRoutine.js
@@ -3,15 +3,17 @@
  * @author MonkeysHK <https://github.com/MonkeysHK>
  * @description A web time engine for the time system in Hypixel SkyBlock.
  * @license GPL-3.0-or-later GNU General Public License v3.0 or later <https://www.gnu.org/licenses/gpl-3.0-standalone.html>
- * @version beta
+ * @version beta_2
  */
 /**
  * **SkyRoutine**  
- * Helper Class: Defines one routine for SkyScheduler.  
+ * A routine handler for both SkyBlock Time and UTC Time.  
  * Dependencies: SkyDuration, SkyDate  
  */
 class SkyRoutine {
     /* INDEX */
+    tasksId = -1
+    tasks = [] // array of { event: <any STATES>, cb: function }
     timeoutStack = []
     constructor(str) {
         this.definition = str;
@@ -21,52 +23,94 @@ class SkyRoutine {
         let data = SkyRoutine.routineTextParser(this.definition);
         // Calculations
         let chosenLocale = checkLocale(data.anchor) || LOCALES.sbst;
+        let i;
         this.anchor = new SkyDate(data.anchor); // a SkyDate used as number
-        this.duration = data.duration !== "" && !isNaN(data.duration) ? new SkyDuration(chosenLocale, data.duration) : new SkyDuration(data.duration || 0); // a SkyDuration used as number
-        this.interval = data.interval !== "" && !isNaN(data.interval) ? new SkyDuration(chosenLocale, data.interval) : new SkyDuration(data.interval || 0); // a SkyDuration used as number
-        if (this.interval < 1 || !isNaN(data.limit))
-            this.limit = this.interval < 1 ? 1 /* no interval: force limit of 1 */ : Number(data.limit);
+        this.totalduration = this.totalbreak = this.cycleExecutions = this.routineExecutions = 0;
+        this.routinePtr = -1;
+        // Handle Cycle
+        this.cycle = (data.cycle || "0|0").split("|");
+        if (this.cycle.length % 2 === 1)
+            this.cycle.push(0);
+        for (i = 0; i < this.cycle.length; i++) {
+            let v = this.cycle[i].toString().trim();
+            this.cycle[i] = v !== "" && !isNaN(v) ? new SkyDuration(chosenLocale, v) : new SkyDuration(v || 0); // a SkyDuration used as number
+            this.totalduration += i % 2 === 0 ? this.cycle[i].valueOf() : 0;
+            this.totalbreak += i % 2 === 1 ? this.cycle[i].valueOf() : 0;
+        }
+        this.cycleTime = this.totalduration + this.totalbreak;
+        this.executeOnce = this.cycleTime < 1;
+        // Handle Limits
+        if (this.executeOnce)
+            this.limit = 1; /* no cycle length: force limit of 1 */
+        if (!isNaN(data.limit)) {
+            this.limit = Number(data.limit);
+            this.cycleLimit = Math.floor(this.limit / (this.cycle.length / 2));
+        }
         if (!!data.until)
             this.until = new SkyDate(data.until); // a SkyDate used as number
         // Get Initial State
-        let current_date = new SkyDate();
-        // execution_count and last_start may be invalid when not started, no interval
-        this.execution_count = Math.max(0, Math.floor(((this.until && (this.until < current_date) ? this.until : current_date) - this.anchor) / this.interval) + 1);
-        if (this.limit)
-            this.execution_count = Math.min(this.limit, this.execution_count);
-        let last_start = this.anchor + (this.execution_count - 1) * (this.interval || 0);
-        if (this.interval >= 1 && this.duration > this.interval)
-            this.duration = this.interval; // duration can't be greater than interval (if exists)
-        // in the case with no interval, it's either not started (cond. 1) or executed once (cond. 2), with no fall through to where interval or last_start is used
-        if (current_date < this.anchor) { // cond. 1: not started
-            this.current_state = SCHEDULE_STATES.WAITING;
-            this.next_start = this.anchor;
-            this.next_end = this.anchor + this.duration;
-        } else if (this.interval < 1 || // cond. 2: no interval and executed once
-            (this.limit && this.limit >= 0 && this.execution_count >= this.limit) || // cond. 3: execution limit reached
-            (this.until && current_date >= this.until)) { // cond. 4: date limit reached
-            if ((last_start || this.anchor) + this.duration < current_date) { // cond: not ongoing
-                this.current_state = SCHEDULE_STATES.STOPPED;
-            } else {
-                this.current_state = SCHEDULE_STATES.ONGOING;
-                this.next_end = (last_start || this.anchor) + this.duration;
+        let currentDate = new SkyDate();
+        if (currentDate < this.anchor) { // cond. 1: not started; cycleExecutions and routineExecutions remains 0
+            this.currentState = SCHEDULE_STATES.WAITING;
+            this.nextStart = this.anchor;
+            this.nextEnd = this.anchor + this.duration;
+        } else {
+            // seek lastCycleStart, lastRoutineStart, cycleExecutions, routineExecutions
+            let lastCycleStart, lastRoutineStart;
+            if (this.executeOnce) {
+                this.cycleExecutions = 1;
+                lastRoutineStart = lastCycleStart = this.anchor;
             }
-        } else { // cond. 5: no limits reached
-            if (last_start + this.duration < current_date) { // cond: not ongoing
-                this.current_state = SCHEDULE_STATES.WAITING;
-                this.next_start = last_start + this.interval;
-                this.next_end = last_start + this.interval + this.duration;
-            } else {
-                this.current_state = SCHEDULE_STATES.ONGOING;
-                this.next_end = last_start + this.duration;
-                this.next_start = last_start + this.interval;
+            else {
+                this.cycleExecutions = Math.floor(((this.until && (this.until < currentDate) ? this.until : currentDate) - this.anchor) / this.cycleTime + 1);
+                if (this.cycleLimit)
+                    this.cycleExecutions = Math.min(this.cycleLimit, this.cycleExecutions);
+                lastRoutineStart = lastCycleStart = this.anchor + (this.cycleExecutions - 1) * this.cycleTime;
+            }
+            let doubleRoutineCount = (this.cycleExecutions - 1) * this.cycle.length;
+            // console.log("Seeing function check:", this.anchor, this.cycleExecutions, this.cycleTime, lastCycleStart, this.totalduration, this.totalbreak);
+            let cumulated = 0;
+            for (i = 0; i < this.cycle.length; i++) {
+                if (lastRoutineStart + (cumulated + this.cycle[i]) > currentDate) {
+                    // this.routinePtr is only valid (> 0) when a routine will happen across the current date
+                    this.routinePtr = i;
+                    doubleRoutineCount += i;
+                    lastRoutineStart = lastCycleStart + cumulated;
+                    break;
+                }
+                cumulated += this.cycle[i];
+            }
+            this.routineExecutions = Math.floor(doubleRoutineCount / 2) + 1;
+            if (this.limit)
+                this.routineExecutions = Math.min(this.limit, this.routineExecutions);
+
+            if (this.executeOnce || // cond. 2: cycle length 0 and executed once
+                (this.limit && this.limit >= 0 && this.routineExecutions >= this.limit) || // cond. 3: execution limit reached
+                (this.until && currentDate >= this.until)) { // cond. 4: date limit reached
+                if (this.routinePtr % 2 === 1 || lastRoutineStart + this.getPeriod() < currentDate) { // cond: not ongoing
+                    this.currentState = SCHEDULE_STATES.STOPPED;
+                } else {
+                    this.currentState = SCHEDULE_STATES.ONGOING;
+                    this.nextEnd = lastRoutineStart + this.getPeriod();
+                }
+            } else { // cond. 5: no limits reached
+                if (this.routinePtr % 2 === 1) { // cond: not ongoing
+                    this.currentState = SCHEDULE_STATES.WAITING;
+                    this.nextStart = lastRoutineStart + this.getPeriod();
+                    this.nextEnd = this.nextStart + this.getPeriod(1);
+                } else {
+                    this.currentState = SCHEDULE_STATES.ONGOING;
+                    this.nextEnd = lastRoutineStart + this.getPeriod();
+                    this.nextStart = this.nextEnd + this.getPeriod(1);
+                }
             }
         }
-        // console.log("Basic information:", this.anchor.valueOf(), this.interval.valueOf(), this.duration.valueOf())
-        // console.log("Limits:", this.execution_count, current_date.valueOf(), (this.until || 0).valueOf(), this.limit)
-        // console.log("State information:", this.current_state, last_start, this.next_start, this.next_end);
+        // console.log("Seeking:", lastCycleStart, lastRoutineStart, this.cycleExecutions, this.routineExecutions)
+        // console.log("Basic information:", this.anchor.valueOf(), this.cycle.map(v => v.valueOf()));
+        // console.log("Limits:", currentDate.valueOf(), this.executeOnce, (this.until || 0).valueOf(), this.limit);
+        // console.log("State information:", this.currentState, this.nextStart, this.nextEnd);
         // Trigger event
-        if (this.current_state === SCHEDULE_STATES.ONGOING)
+        if (this.currentState === SCHEDULE_STATES.ONGOING)
             this.onEventStart(true);
         else
             this.onEventEnd();
@@ -79,49 +123,70 @@ class SkyRoutine {
     removeTimeout(id) {
         this.timeoutStack.splice(this.timeoutStack.indexOf(id), 1);
     }
-    attachScheduler(dispatcher, id) {
-        this.scheduler = dispatcher;
-        this.schedulerIndex = id;
-    }
-    reportToScheduler() {
-        if (this.scheduler) {
-            this.scheduler.triggerGrandState(this.schedulerIndex, this.current_state);
+    addEvent(eventState, callback) {
+        if (Object.values(SCHEDULE_STATES).includes(eventState)) {
+            this.tasksId++;
+            this.tasks[this.tasksId] = {
+                event: eventState,
+                cb: callback
+            };
+            if (this.currentState === eventState)
+                callback(this.currentState);
+            return this.tasksId;
         }
+    }
+    removeEvent(id) {
+        if (id in tasks) {
+            delete this.tasks[id];
+            return true;
+        }
+        return false;
+    }
+    getPeriod(forward) {
+        if (this.routinePtr < 0)
+            return 0;
+        return this.cycle[(this.routinePtr + (forward || 0)) % this.cycle.length];
+    }
+    advancePeriod(forward) {
+        this.routinePtr = (this.routinePtr + (forward || 0)) % this.cycle.length;
     }
     onEventStart(noStateChanges) {
         // State Changes
         if (!noStateChanges) {
-            this.next_start = this.next_start + this.interval; // Note: Calculation dependent on last state
-            if ((this.limit && this.execution_count >= this.limit) || // reached execution limit
-                (this.until && this.next_start >= this.until)) // next start time reached/over date limit
-                this.next_start = null;
-            this.current_state = SCHEDULE_STATES.ONGOING;
-            this.execution_count++;
+            this.nextStart = this.nextEnd + this.getPeriod(1); // Note: Calculation dependent on last state
+            if ((this.limit && this.routineExecutions >= this.limit) || // reached execution limit
+                (this.until && this.nextStart >= this.until)) // next start time reached/over date limit
+                this.nextStart = null;
+            this.currentState = SCHEDULE_STATES.ONGOING;
+            this.advancePeriod(1);
+            this.routineExecutions++;
+            this.cycleExecutions += this.routinePtr === 0 ? 1 : 0;
         }
-        // Report State
-        this.reportToScheduler();
+        // Call Tasks
+        this.callEventSet(SCHEDULE_STATES.ONGOING);
         // Activate Next
         // console.log("onEventStart passing the ball to onEventEnd")
         // console.log(this.next_end, this.next_start)
-        this.passTheBall(this.onEventEnd.bind(this), Math.min(this.next_end, this.next_start /* smallest comes first: duration/interval */ ));
+        this.passTheBall(this.onEventEnd.bind(this), this.nextEnd);
     }
     onEventEnd() {
-        if (!this.next_start) {
+        if (!this.nextStart) {
             // State Changes
-            this.next_end = null;
-            this.current_state = SCHEDULE_STATES.STOPPED;
-            // Report State
-            this.reportToScheduler();
+            this.nextEnd = null;
+            this.currentState = SCHEDULE_STATES.STOPPED;
+            // Call Tasks
+            this.callEventSet(SCHEDULE_STATES.STOPPED);
         } else {
             // State Changes
-            this.next_end = this.next_start + this.duration;
-            this.current_state = SCHEDULE_STATES.WAITING;
-            // Report State
-            this.reportToScheduler();
+            this.nextEnd = this.nextStart + this.getPeriod(1);
+            this.currentState = SCHEDULE_STATES.WAITING;
+            this.advancePeriod(1);
+            // Call Tasks
+            this.callEventSet(SCHEDULE_STATES.WAITING);
             // Activate Next
             // console.log("onEventEnd passing the ball to onEventStart")
             // console.log(this.next_start)
-            this.passTheBall(this.onEventStart.bind(this), this.next_start);
+            this.passTheBall(this.onEventStart.bind(this), this.nextStart);
         }
     }
     passTheBall(callback, startSkyDate) {
@@ -138,11 +203,42 @@ class SkyRoutine {
             _this.timeoutStack.push(tout);
         }
     }
+    callEventSet(eventset) {
+        // console.log("Calling event set: " + eventset);
+        for (let i in this.tasks)
+            if (this.tasks[i].event === eventset)
+                this.tasks[i].cb(this.currentState);
+    }
+    startCountdown(callback) {
+        let _this = this;
+        // align to system clock
+        let countTo = Math.min(this.nextStart, this.nextEnd) / RATIOS.magic,
+            countToDate = new SkyDate(new SkyDuration(LOCALES.utc, countTo));
+        let alignTout = setTimeout(function () {
+            clearTimeout(alignTout);
+            // now start actual countdown
+            let registered = false;
+            let countdownIntr = setInterval(function () {
+                if (!registered) {
+                    _this.timeoutStack.push(countdownIntr);
+                    registered = true;
+                }
+                let now = Date.now() / 1000 - SKYBLOCK_EPOCH.UNIX_TS_UTC;
+                let utcSecondsRemain = Math.floor(countTo - now);
+                if (utcSecondsRemain <= 0)
+                    clearTimeout(countdownIntr);
+                callback(utcSecondsRemain, countdownIntr, countToDate, _this.currentState);
+            }, 1000);
+            // call it the first time
+            let now = Date.now() / 1000 - SKYBLOCK_EPOCH.UNIX_TS_UTC;
+            let utcSecondsRemain = Math.floor(countTo - now);
+            callback(utcSecondsRemain, countdownIntr, countToDate, _this.currentState);
+        }, (new Date()).valueOf() % 1000);
+    }
     static routineTextParser(str) {
         let match;
         return {
-            duration: (match = str.match(/(?:\s|^)T\[(.*?)\]/)) ? match[1] : 0, // a duration expr or number
-            interval: (match = str.match(/(?:\s|^)I\[(.*?)\]/)) ? match[1] : undefined, // a duration expr or number
+            cycle: (match = str.match(/(?:\s|^)C\[(.*?)\]/)) ? match[1] : undefined, // pipe-separated list of duration expr or number
             limit: (match = str.match(/(?:\s|^)L\[(.*?)\]/)) ? match[1] : undefined, // a number
             until: (match = str.match(/(?:\s|^)U\[(.*?)\]/)) ? match[1] : undefined, // a date expr
             anchor: (match = str.match(/(?:\s|^)A\[(.*?)\]/)) ? match[1] : undefined // a date expr
